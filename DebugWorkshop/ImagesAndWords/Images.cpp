92647691fb48da39ad9df5aa8841aadcdc8328ba
shlomoden <shlomoden02@gmail.com>
2024-12-11 19:51:16 +0200

    Added Image and words

--- DebugWorkshop/ImagesAndWords/Images.cpp
+++ DebugWorkshop/ImagesAndWords/Images.cpp
@@ -49,7 +49,7 @@ Image *ReadImage(const char* filename)
 	is.read((char*)&img->header, sizeof(ImageHeader));
 
 	// calculate image size
-	uint16_t imgsize = img->header.width * img->header.height;
+	int imgsize = img->header.width * img->header.height;
 	img->data = new char[imgsize];
 
 	// read image content
@@ -93,7 +93,7 @@ Image *GenerateDummyImage(uint16_t width, uint16_t height)
 
 int main()
 {
-	Image *im = ReadImage("img1.magi");
+	Image *im = ReadImage("img2.magi");
 	FreeImage(im);
 
 	return 0;
