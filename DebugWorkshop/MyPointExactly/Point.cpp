3013c664560e7a7a1505c115fcee8991f3067f06
shlomoden <shlomoden02@gmail.com>
2024-12-11 19:29:09 +0200

    The password is any 15 char string then 0. Ex: 12345678990123450

---
+++ DebugWorkshop/MyPointExactly/Point.cpp
@@ -0,0 +1,54 @@
+#include "Point.h"
+#include <iostream>
+
+Point::Point()
+{
+	_coord = new int[2];
+	_coord[0] = _coord[1] = 0;
+}
+
+Point::Point(int x, int y)
+{
+	_coord = new int[2];
+	_coord[0] = x;
+	_coord[1] = y;
+}
+
+Point::Point(const Point& other)
+{
+	delete _coord;
+	*this = other;
+}
+
+Point::~Point()
+{
+	delete _coord;
+}
+
+Point& Point::operator=(const Point& other)
+{
+	if (this != &other)
+	{
+		delete _coord;
+		_coord = new int[2];
+		memcpy(_coord, other._coord, 2 * sizeof(int));
+	}
+	return *this;
+}
+
+int Point::getX() const 
+{ 
+	return _coord[0]; 
+}
+
+int Point::getY() const
+{ 
+	return _coord[1]; 
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{
+	os << "(" << p._coord[0] << "," << p._coord[1] << ")";
+	return os;
+}
